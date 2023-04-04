b642584c1ffc7e598b250bc44885e95c7e89a332
Tagir <tagir2911@mail.ru>
2023-04-05 00:54:56 +0300

    + models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Case(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    start_time = models.DateTimeField()
+    remind_time = models.DateTimeField()
+    is_repeatable = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
